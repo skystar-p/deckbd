cd2d4ef44c0815b8e62bf18bb3e7a9e7c6863820
Jaehyeon Park <skystar@skystar.dev>
2024-07-07 20:58:45 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,33 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  outputs = { self, nixpkgs }:
-    let sys = "x86_64-linux";
-    in {
-      packages.${sys}.default = with nixpkgs.legacyPackages.${sys}; stdenv.mkDerivation {
-        name = "deckbd";
-        src = ./.;
-        buildInputs = [ libevdev glib.dev ];
-        nativeBuildInputs = [ pkg-config ];
-        makeFlags = [ "PREFIX=$(out)" ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "deckbd";
+          src = ./.;
+          buildInputs = with pkgs; [
+            libevdev
+            glib.dev
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          makeFlags = [ "PREFIX=$(out)" ];
+        };
       };
-    };
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          libevdev
+          glib.dev
+        ];
+      };
+    });
 }
