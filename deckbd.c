4204469fffb04e0e38f1c3f02a59307f57ee9f54
Jaehyeon Park <skystar@skystar.dev>
2024-07-07 21:16:48 +0900

    fmt

--- deckbd.c
+++ deckbd.c
@@ -4,62 +4,59 @@
 #include <linux/input.h>
 #include <signal.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <libevdev/libevdev.h>
-#include <libevdev/libevdev-uinput.h>
 
-#define ARRLEN(s) (sizeof(s)/sizeof(*s))
+#include <libevdev/libevdev-uinput.h>
+#include <libevdev/libevdev.h>
+
+#define ARRLEN(s) (sizeof(s) / sizeof(*s))
 
 struct managed_device {
-  struct libevdev* controller;
-  struct libevdev* input;
-  struct libevdev_uinput* uinput;
+  struct libevdev *controller;
+  struct libevdev *input;
+  struct libevdev_uinput *uinput;
 };
 
 const uint8_t modifiers[] = {
-  KEY_LEFTSHIFT,
-  KEY_LEFTCTRL,
-  KEY_LEFTALT,
-  KEY_RIGHTSHIFT,
-  KEY_RIGHTCTRL,
-  KEY_RIGHTALT,
+    KEY_LEFTSHIFT,  KEY_LEFTCTRL,  KEY_LEFTALT,
+    KEY_RIGHTSHIFT, KEY_RIGHTCTRL, KEY_RIGHTALT,
 };
-#define MOD_LEFTSHIFT  (0x1)
-#define MOD_LEFTCTRL   (0x1 << 1)
-#define MOD_LEFTALT    (0x1 << 2)
+#define MOD_LEFTSHIFT (0x1)
+#define MOD_LEFTCTRL (0x1 << 1)
+#define MOD_LEFTALT (0x1 << 2)
 #define MOD_RIGHTSHIFT (0x1 << 3)
-#define MOD_RIGHTCTRL  (0x1 << 4)
-#define MOD_RIGHTALT   (0x1 << 5)
+#define MOD_RIGHTCTRL (0x1 << 4)
+#define MOD_RIGHTALT (0x1 << 5)
 
 struct mapping {
   uint16_t button;
   uint16_t modifier;
   uint16_t key;
-  bool     pressed;
+  bool pressed;
 } mappings[] = {
-  { .button = BTN_DPAD_UP,    .modifier = 0, .key = KEY_1, .pressed = false },
-  { .button = BTN_DPAD_LEFT,  .modifier = 0, .key = KEY_2, .pressed = false },
-  { .button = BTN_DPAD_DOWN,  .modifier = 0, .key = KEY_3, .pressed = false },
-  { .button = BTN_DPAD_RIGHT, .modifier = 0, .key = KEY_4, .pressed = false },
+    {.button = BTN_DPAD_UP, .modifier = 0, .key = KEY_1, .pressed = false},
+    {.button = BTN_DPAD_LEFT, .modifier = 0, .key = KEY_2, .pressed = false},
+    {.button = BTN_DPAD_DOWN, .modifier = 0, .key = KEY_3, .pressed = false},
+    {.button = BTN_DPAD_RIGHT, .modifier = 0, .key = KEY_4, .pressed = false},
 
-  { .button = BTN_Y, .modifier = 0, .key = KEY_5, .pressed = false },
-  { .button = BTN_X, .modifier = 0, .key = KEY_6, .pressed = false },
-  { .button = BTN_A, .modifier = 0, .key = KEY_7, .pressed = false },
-  { .button = BTN_B, .modifier = 0, .key = KEY_8, .pressed = false },
+    {.button = BTN_Y, .modifier = 0, .key = KEY_5, .pressed = false},
+    {.button = BTN_X, .modifier = 0, .key = KEY_6, .pressed = false},
+    {.button = BTN_A, .modifier = 0, .key = KEY_7, .pressed = false},
+    {.button = BTN_B, .modifier = 0, .key = KEY_8, .pressed = false},
 
-  { .button = BTN_TL, .modifier = 0, .key = KEY_9, .pressed = false },
-  { .button = BTN_TR, .modifier = 0, .key = KEY_0, .pressed = false },
+    {.button = BTN_TL, .modifier = 0, .key = KEY_9, .pressed = false},
+    {.button = BTN_TR, .modifier = 0, .key = KEY_0, .pressed = false},
 
-  { .button = BTN_TL2, .modifier = 0, .key = KEY_BACKSPACE, .pressed = false },
-  { .button = BTN_TR2, .modifier = 0, .key = KEY_ENTER,     .pressed = false },
+    {.button = BTN_TL2, .modifier = 0, .key = KEY_BACKSPACE, .pressed = false},
+    {.button = BTN_TR2, .modifier = 0, .key = KEY_ENTER, .pressed = false},
 };
 
-bool starts_with(const char* prefix, const char* str){
+bool starts_with(const char *prefix, const char *str) {
   return strncmp(prefix, str, strlen(prefix)) == 0;
 }
 
@@ -69,19 +66,19 @@ int find_controller() {
   const uint16_t SD_PID = 0x1205;
 
   int fd = -1;
-  DIR* devs = opendir("/dev/input");
+  DIR *devs = opendir("/dev/input");
   if (devs == NULL) {
     g_debug("Failed to open /dev/input");
     return -1;
   }
 
-  struct dirent* dev;
+  struct dirent *dev;
   while ((dev = readdir(devs)) != NULL) {
     if (dev->d_type == DT_CHR && starts_with("event", dev->d_name)) {
       // MAX_LEN(d_name) == 256, strlen("/dev/input/") == 11
       char pre[268] = "/dev/input/";
-      char* path = strcat(pre, dev->d_name);
-      
+      char *path = strcat(pre, dev->d_name);
+
       fd = open(path, O_RDONLY | O_NONBLOCK);
       if (fd < 0) {
         g_debug("Failed to open %s", path);
@@ -90,13 +87,14 @@ int find_controller() {
         int rc;
 
         rc = ioctl(fd, EVIOCGID, &info);
-        if (rc < 0){
+        if (rc < 0) {
           g_debug("Failed to get device info of %s", path);
           goto cleanup;
         }
 
-        if (info.bustype == SD_TYPE && info.vendor == SD_VID && info.product == SD_PID) {
-          struct libevdev* dev = NULL;
+        if (info.bustype == SD_TYPE && info.vendor == SD_VID &&
+            info.product == SD_PID) {
+          struct libevdev *dev = NULL;
           rc = libevdev_new_from_fd(fd, &dev);
           if (rc < 0) {
             g_debug("Failed to init libevdev for %s", path);
@@ -123,24 +121,25 @@ final:
   return fd;
 }
 
-struct managed_device init_evdev(int fd){
-  struct managed_device device = { NULL, NULL, NULL };
-  struct libevdev* controller = NULL;
+struct managed_device init_evdev(int fd) {
+  struct managed_device device = {NULL, NULL, NULL};
+  struct libevdev *controller = NULL;
   int rc = libevdev_new_from_fd(fd, &controller);
-  if (rc != 0){
+  if (rc != 0) {
     g_debug("Failed to create libevdev.");
     return device;
   }
-  
-  struct libevdev* input = libevdev_new();
+
+  struct libevdev *input = libevdev_new();
   libevdev_set_name(input, "deckbd");
   libevdev_enable_event_type(input, EV_KEY);
   for (size_t i = 0; i < ARRLEN(mappings); i++) {
     libevdev_enable_event_code(input, EV_KEY, mappings[i].key, NULL);
   };
 
-  struct libevdev_uinput* uinput = NULL;
-  rc = libevdev_uinput_create_from_device(input, LIBEVDEV_UINPUT_OPEN_MANAGED, &uinput);
+  struct libevdev_uinput *uinput = NULL;
+  rc = libevdev_uinput_create_from_device(input, LIBEVDEV_UINPUT_OPEN_MANAGED,
+                                          &uinput);
   if (rc != 0) {
     libevdev_free(controller);
     libevdev_free(input);
@@ -161,13 +160,13 @@ void free_evdev(struct managed_device device) {
 }
 
 void press(struct managed_device device, uint16_t modifier, uint32_t code) {
-  struct libevdev_uinput* uinput = device.uinput;
+  struct libevdev_uinput *uinput = device.uinput;
 
   for (size_t i = 0; i < ARRLEN(modifiers); i++) {
     if (((0x1 << i) & modifier) != 0) {
       libevdev_uinput_write_event(uinput, EV_KEY, modifiers[i], 1);
       libevdev_uinput_write_event(uinput, EV_SYN, SYN_REPORT, 0);
-    } 
+    }
   }
 
   libevdev_uinput_write_event(uinput, EV_KEY, code, 1);
@@ -179,14 +178,15 @@ void press(struct managed_device device, uint16_t modifier, uint32_t code) {
     if (((0x1 << i) & modifier) != 0) {
       libevdev_uinput_write_event(uinput, EV_KEY, modifiers[i], 0);
       libevdev_uinput_write_event(uinput, EV_SYN, SYN_REPORT, 0);
-    } 
+    }
   }
 }
 
 void handle_event(struct managed_device device, struct input_event ev) {
   for (size_t i = 0; i < ARRLEN(mappings); i++) {
-    struct mapping* entry = &mappings[i];
-    if (entry->button != ev.code) continue;
+    struct mapping *entry = &mappings[i];
+    if (entry->button != ev.code)
+      continue;
 
     if (ev.value == 1) {
       entry->pressed = true;
@@ -196,20 +196,18 @@ void handle_event(struct managed_device device, struct input_event ev) {
       entry->pressed = false;
       press(device, entry->modifier, entry->key);
     }
-    
+
     return;
   }
 };
 
 volatile bool listening = true;
-void sig_handler(int signum) {
-  listening = false;
-};
+void sig_handler(int signum) { listening = false; };
 int init_signal_handler() {
   int rc;
-  struct sigaction act = { 0 };
+  struct sigaction act = {0};
   act.sa_handler = &sig_handler;
-  
+
   if ((rc = sigaction(SIGINT, &act, NULL)) != 0) {
     return rc;
   }
@@ -221,7 +219,7 @@ int init_signal_handler() {
   return 0;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc > 1 && (strcmp("query", argv[1]) == 0)) {
     int fd = find_controller();
     if (fd < 0) {
@@ -250,18 +248,19 @@ int main(int argc, char* argv[]) {
     g_debug("Device initialisation failed.");
     close(fd);
     return 1;
-  } 
+  }
 
   g_debug("Listening...");
   struct input_event ev;
-  while(listening) {
+  while (listening) {
     rc = libevdev_next_event(device.controller, LIBEVDEV_READ_FLAG_NORMAL, &ev);
     if (rc == 0 && ev.type == EV_KEY) {
-      g_debug("pressed type: %x, code: %x, value: %x", ev.type, ev.code, ev.value);
+      g_debug("pressed type: %x, code: %x, value: %x", ev.type, ev.code,
+              ev.value);
       handle_event(device, ev);
     }
   }
-  
+
   free_evdev(device);
   g_debug("Deactivated.");
   return 0;
